8818fff9a587a4b9e93cebd32969e1093ae93c25
MichalJozef <47686920+MichalJozef@users.noreply.github.com>
2021-04-19 13:24:32 +0000

    Add files via upload

---
+++ web/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+
+export class AppComponent {
+  
+  imageUrl: string = "../assets/img/stars.svg-a4505-259.png"
+}
